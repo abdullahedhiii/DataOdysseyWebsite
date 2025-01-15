c4c8d4d81cc2d87730f8365f5ffabf626d3ce925
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-01-15 14:18:50 +0500

    added submitted at timestamp in solution table

--- database.sql
+++ database.sql
@@ -27,11 +27,11 @@ CREATE TABLE solutions (
     CONSTRAINT QuestionRef FOREIGN KEY (queryId) REFERENCES queries(queryId)
 );
 ALTER TABLE solutions AUTO_INCREMENT  = 4392;
+ALTER TABLE solutions ADD COLUMN submitted_at DATETIME default current_timestamp;
 
 insert into queries (title, description, level, difficulty) values ('Tracking Time Efficiency of Top Participants','To highlight time efficiency, the organizers want to find the fastest participant in solving at least 5 problems while maintaining a position in the top 10 scorers.',1,'Easy');
 insert into queries (title, description, level, difficulty) values ('Identifying the Clutch Performers','During the competition, organizers want to identify users who made a significant comeback by being outside the top 10 in the first half but finishing in the top 5 by the end. This will highlight participants who showed exceptional performance under pressure.',1,'Medium');
 insert into queries (title, description, level, difficulty) values ('Detecting Problem Masters','The organizers want to reward users who solved the hardest problems (those solved by the least number of participants). The query should list the hardest problems and the users who solved them.',1,'Hard');
 
+
 drop table solutions;
-drop table queries;
-select * from queries;
