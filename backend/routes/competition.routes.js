076ce9188ee52374b0b3a8d93e834ea4f10f6bb5
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-14 17:46:20 +0500

    removed multer config

--- backend/routes/competition.routes.js
+++ backend/routes/competition.routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {sendQueries,submitQuery, sendLeaderboardData} = require('../controllers/competition.controller');
-const uploadMiddleware = require('../config/multerConfig'); 
 
 router.get('/queries/:id',sendQueries)
 router.get('/leaderboardData',sendLeaderboardData)
-// router.post('/submitFile',uploadMiddleware,submitQuery);
 router.post('/submitFile',submitQuery);
 
 
